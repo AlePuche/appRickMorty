128134ae88d0205e0af2998d095eec14b0294db4
AlePuche <alepucheperez@gmail.com>
2024-10-15 16:37:25 +0200

    eliminacion lineas innecesarias

--- lib/blocs/character_event.dart
+++ lib/blocs/character_event.dart
@@ -1,23 +1,4 @@
-import 'package:equatable/equatable.dart';
+abstract class CharacterEvent {}
 
-// Clase abstracta base para los eventos
-abstract class CharacterEvent extends Equatable {
-  const CharacterEvent();
-
-  @override
-  List<Object> get props => [];
-}
-
-// Evento para cargar los personajes inicialmente
 class LoadCharactersEvent extends CharacterEvent {}
-
-// Evento para cargar más personajes al hacer scroll
-class LoadMoreCharactersEvent extends CharacterEvent {
-  final int offset;
-
-  LoadMoreCharactersEvent(this.offset);
-
-  @override
-  List<Object> get props => [offset];
-}
 
